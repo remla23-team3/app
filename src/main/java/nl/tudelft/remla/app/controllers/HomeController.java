86f36f46d949bbcfa32320cdae8811f40d9253fa
Simcha Vos <simchavos.bedrijven@gmail.com>
2023-05-08 16:00:12 +0200

    Update HomeController, pom.xml and application.properties

--- src/main/java/nl/tudelft/remla/app/controllers/HomeController.java
+++ src/main/java/nl/tudelft/remla/app/controllers/HomeController.java
@@ -1,16 +1,17 @@
 package nl.tudelft.remla.app.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
 
 	public HomeController(){}
 
 	@GetMapping("/home")
 	public String home() {
-		return "static/index.html";
+		System.out.println("test");
+		return "index";
 	}
 
 }
