b70db53ff36515205a8d545e826e4a5abb65ebbb
Simcha Vos <simchavos.bedrijven@gmail.com>
2023-05-08 13:22:55 +0200

    Update pom.xml file so mvn clean install works. Added HomeController for home page index.html. WIP to get the home page working

---
+++ src/main/java/nl/tudelft/remla/app/controllers/HomeController.java
@@ -0,0 +1,16 @@
+package nl.tudelft.remla.app.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+	@GetMapping("/")
+	public String home() {
+		return "static/index.html";
+	}
+
+}
+
