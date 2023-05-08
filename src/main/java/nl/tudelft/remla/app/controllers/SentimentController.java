5b266d63c14ae53824dac6407be3654c0f567ef5
Simcha Vos <simchavos.bedrijven@gmail.com>
2023-05-08 18:04:35 +0200

    Implement showResult to show the result of sentiment analysis. Unfortunately it picks the static html files. It should be picking up the html files in templates folder

--- src/main/java/nl/tudelft/remla/app/controllers/SentimentController.java
+++ src/main/java/nl/tudelft/remla/app/controllers/SentimentController.java
@@ -8,10 +8,11 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class SentimentController {
@@ -19,22 +20,42 @@ public class SentimentController {
 	@Value("${modelServiceUrl}")
 	private String modelServiceUrl;
 
-	@PostMapping(value = "/sentiment", consumes = MediaType.APPLICATION_JSON_VALUE)
-	@ResponseBody
-	public SentimentResponse getSentiment(@RequestBody SentimentRequest request) {
-		// Set the headers for the request
-		HttpHeaders headers = new HttpHeaders();
-		headers.setContentType(MediaType.APPLICATION_JSON);
+	@PostMapping(value = "/sentiment", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+//	@ResponseBody
+	public RedirectView handleSentimentRequest(@ModelAttribute SentimentRequest request, RedirectAttributes redirectAttributes) {
+		System.out.println(request);
 
-		// Set the request entity
-		HttpEntity<SentimentRequest> entity = new HttpEntity<>(request, headers);
+		// Call the service to get the sentiment analysis result
+		String sentiment = "sad";
 
-		// Send the POST request to the model-service and get the response
-		RestTemplate restTemplate = new RestTemplate();
-		ResponseEntity<SentimentResponse> response = restTemplate.exchange(modelServiceUrl, HttpMethod.POST, entity, SentimentResponse.class);
+		// Add the sentimentRequest and sentiment attributes to the model
+//		model.addAttribute("sentimentRequest", request);
+//		model.addAttribute("sentiment", sentiment);
+		redirectAttributes.addFlashAttribute("sentimentRequest", request);
+		redirectAttributes.addFlashAttribute("sentiment", sentiment);
+		// Return the name of the view
+		return new RedirectView("/showResult");
+	}
+
+	@GetMapping("/showResult")
+	public String showResult(Model model) {
+		model.addAttribute("sentimentRequest", new SentimentRequest());
+		model.addAttribute("sentiment", "sad");
+		return "result";
+	}
+//		HttpHeaders headers = new HttpHeaders();
+//		headers.setContentType(MediaType.APPLICATION_JSON);
+//
+//		// Set the request entity
+//		HttpEntity<SentimentRequest> entity = new HttpEntity<>(request, headers);
+//
+//		// Send the POST request to the model-service and get the response
+//		RestTemplate restTemplate = new RestTemplate();
+//		ResponseEntity<SentimentResponse> response = restTemplate.exchange(modelServiceUrl, HttpMethod.POST, entity, SentimentResponse.class);
+//		return "sad";
 
 		// Return the sentiment response back to the frontend
-		return response.getBody();
-	}
+//		return response.getBody();
+
 }
 
